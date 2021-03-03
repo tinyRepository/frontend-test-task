d4680dea747b16a619b8c272e5cf2bf9d6740ac8
Andrey Solovyev <tinymail8@gmail.com>
2021-03-03 22:41:39 +0300

    add header

---
+++ src/components/HeaderEl.vue
@@ -0,0 +1,48 @@
+<template>
+  <header class="header">
+    <div class="header__content">
+      <div class="header__logo" />
+      <menu-list />
+      <search-el class="header__search" />
+    </div>
+  </header>
+</template>
+
+<script>
+import MenuList from './MenuList';
+import SearchEl from './SearchEl';
+
+export default {
+  components: {
+    MenuList,
+    SearchEl,
+  },
+};
+</script>
+
+<style lang="scss">
+.header {
+  user-select: none;
+  border-bottom: 1px solid $gray-color1;
+
+  &__content {
+    height: $header-height;
+    display: flex;
+    align-items: center;
+    margin: auto;
+    max-width: $content-width;
+    width: 100%;
+  }
+
+  &__logo {
+    background: url('~@/assets/images/logo.jpg') no-repeat;
+    background-size: cover;
+    width: 48px;
+    height: 48px;
+  }
+
+  &__search {
+    margin-left: auto;
+  }
+}
+</style>
---
+++ src/components/MenuList.vue
@@ -0,0 +1,34 @@
+<template>
+  <ul class="menu-list">
+    <li class="menu-list__list-item" v-for="(item, index) in menuItems" :key="index">{{ item }}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      menuItems: ['Каталог', 'Доставка', 'Оплата', 'Контакты', 'О галерее'],
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+.menu-list {
+  display: flex;
+  flex-direction: row;
+  list-style: none;
+  padding-left: 48px;
+
+  &__list-item {
+    @include font(14px, $gray-color2);
+    line-height: 21px;
+    cursor: pointer;
+
+    &:not(:last-child) {
+      margin-right: 48px;
+    }
+  }
+}
+</style>
