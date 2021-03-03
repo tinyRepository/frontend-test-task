d886ae4be54594b62ba3bb37b84158ee120596f6
Andrey Solovyev <tinymail8@gmail.com>
2021-03-03 22:38:47 +0300

    add base styles

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
-import Vue from "vue";
-import App from "./App.vue";
+import Vue from 'vue';
+import App from './App.vue';
+import 'normalize.css';
+import '@/styles/common.scss';
 
 Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App)
-}).$mount("#app");
+  render: h => h(App),
+}).$mount('#app');
