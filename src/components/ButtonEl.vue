25e4198c0aa9d4a0dc3ef37481610025a604b9b4
Andrey Solovyev <tinymail8@gmail.com>
2021-03-06 21:00:30 +0300

    add loading for button

--- src/components/ButtonEl.vue
+++ src/components/ButtonEl.vue
@@ -1,15 +1,21 @@
 <template>
-  <button class="button" :disabled="disabled" v-on="$listeners">
-    <slot></slot>
+  <button
+    class="button"
+    :disabled="disabled"
+    v-on="$listeners"
+    :class="{ 'button_in-cart': inCart, button_loading: loading }"
+  >
+    <div v-if="loading" class="button__loader" />
+    <slot v-else></slot>
   </button>
 </template>
 
 <script>
 export default {
   props: {
-    disabled: {
-      type: Boolean,
-    },
+    loading: Boolean,
+    inCart: Boolean,
+    disabled: Boolean,
   },
 };
 </script>
@@ -24,12 +30,48 @@ export default {
   text-align: center;
   transition: background $transition;
 
+  &__loader {
+    margin: auto;
+    width: 25px;
+    height: 25px;
+    border-radius: 50%;
+    position: relative;
+
+    border-top: 3px solid $white-color;
+    border-left: 3px solid transparent;
+    border-right: 3px solid transparent;
+    border-bottom: 3px solid transparent;
+
+    animation: spin $transition * 2 ease-out infinite;
+  }
+
+  &__check-icon {
+    margin-right: 4px;
+    position: relative;
+    left: 1px;
+    top: 1px;
+  }
+
+  &_in-cart {
+    background: $brown-color4;
+  }
+
+  &_loading {
+    pointer-events: none;
+  }
+
   &[disabled] {
     background: $brown-color3;
   }
 
   &:hover {
     background: $brown-color2;
+  }
+}
+
+@keyframes spin {
+  100% {
+    transform: rotate(360deg);
   }
 }
 </style>
